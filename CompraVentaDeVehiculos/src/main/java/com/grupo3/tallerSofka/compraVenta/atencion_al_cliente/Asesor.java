8527b702ce9bdf540a60476dd3331027c73c01d7
Felipe <fquiceno1@misena.edu.co>
2022-03-08 20:51:59 -0500

    Se crean las clases para las identidades de Asesor, PQRS, Sucursal y se aplican las respectivas herencias de la libreria de sofka, identificando las Entity y los Identify

--- CompraVentaDeVehiculos/src/main/java/com/grupo3/tallerSofka/compraVenta/atencion_al_cliente/Asesor.java
+++ CompraVentaDeVehiculos/src/main/java/com/grupo3/tallerSofka/compraVenta/atencion_al_cliente/Asesor.java
@@ -1,4 +1,11 @@
 package com.grupo3.tallerSofka.compraVenta.atencion_al_cliente;
 
-public class Asesor {
+import co.com.sofka.domain.generic.Entity;
+import com.grupo3.tallerSofka.compraVenta.atencion_al_cliente.Values.AsesorId;
+
+public class Asesor extends Entity<AsesorId> {
+
+    public Asesor(AsesorId entityId) {
+        super(entityId);
+    }
 }
--- CompraVentaDeVehiculos/src/main/java/com/grupo3/tallerSofka/compraVenta/atencion_al_cliente/PQRS.java
+++ CompraVentaDeVehiculos/src/main/java/com/grupo3/tallerSofka/compraVenta/atencion_al_cliente/PQRS.java
@@ -1,4 +1,10 @@
 package com.grupo3.tallerSofka.compraVenta.atencion_al_cliente;
 
-public class PQRS {
+import co.com.sofka.domain.generic.Entity;
+import com.grupo3.tallerSofka.compraVenta.atencion_al_cliente.Values.PQRSId;
+
+public class PQRS extends Entity<PQRSId> {
+    public PQRS(PQRSId entityId) {
+        super(entityId);
+    }
 }
